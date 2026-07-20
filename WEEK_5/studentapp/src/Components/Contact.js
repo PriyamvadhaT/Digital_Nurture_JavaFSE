9004dbe337e0f4f38b1c7957258875bf94810aeb
PriyamvadhaT <anbuselvithiyagu23@gmail.com>
2026-07-20 15:44:04 +0530

    project added to WEEK_5

---
+++ WEEK_5/studentapp/src/Components/Contact.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+    render() {
+        return (
+            <div>
+                <h2><center>Welcome to the Contact page of the Student Management Portal</center></h2>
+            </div>
+        );
+    }
+}
+
+export default Contact;
---
+++ WEEK_5/studentapp/src/Components/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <h2><center>Welcome to the Home page of Student Management Portal</center></h2>
+            </div>
+        );
+    }
+}
+
+export default Home;
